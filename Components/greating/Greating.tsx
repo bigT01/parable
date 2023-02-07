9570871add4c6acdfb0d5dd3fd407dc77530f9f4
Tanat <tanat8511@gmail.com>
2023-02-07 11:59:18 +0600

    fix: pop up

--- Components/greating/Greating.tsx
+++ Components/greating/Greating.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Greating = ({setIsPopUp}:any) => {
     return(
-        <div className="flex flex-col items-center justify-center fixed bg-blue-500 z-20 top-2/4 left-2/4 p-10 rounded-2xl text-white min-w-1/3" style={{transform: 'translate(-50%, -50%)'}}>
+        <div className="flex flex-col items-center justify-center  bg-blue-500 z-20 top-2/4 left-2/4 p-10 rounded-2xl text-white min-w-1/3" style={{transform: 'translate(-50%, -50%)', position:'fixed'}}>
             <Image src='assets/settings.svg' alt='icon-settings' width={500} height={500} className="relative left-2 mb-10"/>
             <h1 className="font-bold text-3xl">Пожалуйста проидите опрос</h1>
             <p className="mb-12">это займет 1-2 минуты</p>
